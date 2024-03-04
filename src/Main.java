b3aecea7a6dc438e111c8e5fffcaad8f94fc02c5
DilkerWinter <146080789+DilkerWinter@users.noreply.github.com>
2024-03-04 13:52:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,10 @@ public class Main {
         JMenuBar menuBar = new MenuBar(); // Usando a classe MenuBar
         telainicio.setJMenuBar(menuBar);
 
+        //Adicionar tela de Text utilizando a classe de Diario.
+
+        //Adicionar botão para customização de texto.
+
         telainicio.revalidate();
         telainicio.repaint();
     }
